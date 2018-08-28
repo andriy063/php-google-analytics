17bd99ff0c4860bacd06bcad99792701904903bf
andriy063 <andriy063@gmail.com>
2018-08-28 16:51:14 +0300

    Update php-google-analytics.php

--- php-google-analytics.php
+++ php-google-analytics.php
@@ -1,1 +1,28 @@
+<?php
+class php_google_analytics {
+  
+  public function send($data) {
+    $url = 'https://ssl.google-analytics.com/collect';
+    $post_string = '?payload_data&';
+    $post_string .= http_build_query($data);
 
+    $parts = parse_url($url);
+    $port = isset($parts['port']) ? $parts['port'] : 80;
+    $success = $fp = fsockopen($parts['host'], $port, $errno, $errstr, 30);
+    
+    if ($fp) {
+      $output = "POST " . $parts['path'] . " HTTP/1.1\r\n";
+      $output .= "Host: " . $parts['host'] . "\r\n";
+      $output .= "Content-Type: application/x-www-form-urlencoded\r\n";
+      $output .= "Content-Length: " . strlen($post_string) . "\r\n";
+      $output .= "Connection: Close\r\n\r\n";
+      $output .= isset($post_string) ? $post_string : '';
+
+      $success = fwrite($fp, $output);
+      fclose($fp);
+    }
+
+    return $success ? true : false;
+  }
+  
+}
